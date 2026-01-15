d60d3735a56c3aa1525e3585400fdd6b338eaec4
fahadkarim <fahadkar33m@icloud.com>
2026-01-15 18:48:00 +0500

    day- 03 of C++

---
+++ Day-03/dataTypes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age =20;                  
+    float price=99.98;          
+    double pie= 3.141592653;  
+    char grade='A';
+    bool isPAssed=true;
+
+
+
+    cout<<"AGE =" <<age << endl;
+    cout<<"The price is ="<<price <<endl;
+    cout<<"The value of pie is ="<<pie <<endl;
+    cout<<"The grade received is ="<<grade <<endl;
+    cout<<"Is the user is passed ="<<isPAssed <<endl;
+    return 0;
+
+}   
+
+
---
+++ Day-03/input/output.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age;
+    float height;
+    char grade;
+
+    cout <<"Enter your age ";
+    cin >>age;
+
+    cout<<"Enter your height";
+    cin>>height;
+
+    cout<<"Enter your grade";
+    cin>>grade;
+
+    cout << endl;
+    cout << "Age :"  << age << endl;
+    cout << "Height :" <<height <<endl;
+    cout << "Grade:"   <<grade<<endl;
+
+    return 0;
+} 
+
+
+
