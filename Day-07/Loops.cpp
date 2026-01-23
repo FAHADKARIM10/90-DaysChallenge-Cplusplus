9a48518020bb3bbbe7a89974d5bc20690dbdd9ea
fahadkarim <fahadkar33m@icloud.com>
2026-01-23 18:56:17 +0500

    conditional statement practicel implementation

---
+++ Day-07/Loops.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i=5;
+    int j=10;
+
+    while(i<=5){
+        cout<<i<<"";
+        i++;
+    }
+
+    do {
+        cout<<j;
+    } while(j<5);
+    
+
+    return 0;  
+}                     //12345
+
