de0ab9b2c6c8e4ed757e92a2c62f5447403d6a9a
fahadkarim <fahadkar33m@icloud.com>
2026-02-06 03:30:11 +0500

    a basic touch of functions in cpp

---
+++ Day-11/functions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void myFunction(){
+    cout<<"hello there";
+}
+
+int main(){
+    myFunction(); // calls the fucnction myFunction 
+    myFunction();
+    myFunction();
+    return 0;
+}
+
+// a function can be called multiple times 
+// myFunction declaration and defination u
